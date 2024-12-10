73b2ada65c496d7489787a1e977017a9efb8d8b0
saifu907 <115540563+saifu907@users.noreply.github.com>
2024-12-10 20:06:38 +0530

    Time: 0 ms (100%), Space: 50 MB (43.11%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,17 +3,12 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    strs.sort()
-    let a = strs[0]
-    let b = strs[strs.length - 1]
-    let c = ''
-    for(let i = 0; i < a.length; i++){
-        if(a[i] == b[i]){
-            c += a[i]
-        }else{
-            break
+  for(var i=0; i<strs[0].length; i++){
+    for(var j=1; j<strs.length; j++){
+        if(strs[0][i] !== strs[j][i]){
+            return strs[0].slice(0, i)
         }
     }
-    return c
-    
+}  
+return strs[0]
 };
